b57cf97eea8256b6531f9e5287063390956c5550
Marcus <marcusaviotti@gmail.com>
2022-08-18 19:11:19 -0300

    Testando a bagunça que eu fiz

---
+++ Fundamentos/Modulo1/Bloco4/Dia-1/pratica1.js
@@ -0,0 +1,8 @@
+const myName = Marcus;
+const birthCity = Paris;
+let birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
---
+++ Fundamentos/Modulo1/Bloco4/Dia-1/pratica3.js
@@ -0,0 +1,12 @@
+const nota = 65;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado!");
+}
+else if (nota < 80 && nota >= 60); {
+    consolel.log("Você está na nossa lista de espera.");
+}
+else {
+    console.log("Você foi reprovado.");
+}
+   
