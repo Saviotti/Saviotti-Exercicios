c9d880fe90a5f617d95abc9fb81ef640bfd0a58e
Marcus <marcusaviotti@gmail.com>
2022-08-18 19:54:13 -0300

    Exercicio 1 feito até a parte 3

---
+++ Fundamentos/Modulo1/Bloco4/Dia-1/exercicio1-1.js
@@ -0,0 +1,8 @@
+const a = 15;
+const b = 10;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
---
+++ Fundamentos/Modulo1/Bloco4/Dia-1/exercicio1-3.js
@@ -0,0 +1,13 @@
+const a = 12;
+const b = 8;
+const c = 25;
+
+if (a > b && a > c) {
+    console.log(a);
+}
+else if (b > a && b > c) {
+    console.log(b)
+}
+else {
+    console.log(c);
+}
