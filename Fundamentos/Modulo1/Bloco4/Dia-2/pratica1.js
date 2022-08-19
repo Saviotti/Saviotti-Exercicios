e597063e48c3216f030ca6e176943ee8cb90b78e
Marcus <marcusaviotti@gmail.com>
2022-08-19 16:23:50 -0300

    Pratica 1 do exercicio For feito

---
+++ Fundamentos/Modulo1/Bloco4/Dia-2/pratica1.js
@@ -0,0 +1,6 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(var indice = 0; indice < groceryList.length; indice += 1) {
+    var lista = groceryList[indice];
+    console.log(lista)
+}
