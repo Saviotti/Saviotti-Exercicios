df29a3882efd5b5e6f34e957dcc8c03993a1518d
Marcus <marcusaviotti@gmail.com>
2022-08-22 19:11:32 -0300

    Exercicio 1 feito, 2 ta na metade

---
+++ Fundamentos/Modulo1/Bloco4/Dia-3/Exercicio1.js
@@ -0,0 +1,15 @@
+let fatorial = 10;
+let resultado = fatorial;
+
+for(let index = 1; index < fatorial; index += 1) {
+    resultado = resultado * index;
+}
+console.log(resultado);
+
+// 
+
+let word = 'tryber';
+let split = word;
+
+split(word);
+reverse(split);
