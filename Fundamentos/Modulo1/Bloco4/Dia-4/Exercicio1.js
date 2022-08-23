ebbde6c3a1dfab93f0dd4bbbe9b45e0076f11d6e
Marcus <marcusaviotti@gmail.com>
2022-08-23 20:02:17 -0300

    Atualizando exerc 1-Dia4

---
+++ Fundamentos/Modulo1/Bloco4/Dia-4/Exercicio1.js
@@ -0,0 +1,30 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+    info2: {
+        personagem: 'Tio Patinhas',
+        origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+        nota: 'O último MacPatinhas',
+        recorrente: 'Sim'
+    },
+  };
+
+//   console.log('Bem vinda, ' + info.personagem);
+
+
+//   ----------3-----------
+// for(let index in info) {
+//     console.log(index);
+// }
+
+// ---------4---------
+// for(let index in info) {
+//     console.log(info[index]);
+// }
+
+// ---------5--------
+for(let index in info2) {
+    console.log(info2[index]);
+}
