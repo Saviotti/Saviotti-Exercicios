65e0998d9c607ac6df06865ef25800febc10dc48
Seu nome <marcusaviotti@gmail.com>
2022-09-22 15:55:03 -0300

    Pratica 1

--- Fundamentos/Seção-8/Dia-1/Pratica 1/HOF.js
+++ Fundamentos/Seção-8/Dia-1/Pratica 1/HOF.js
@@ -1,5 +1,17 @@
 // Ao chamar a função doingThings:
-doingThings(wakeUp);
+
 
 // Ela deve retornar o valor do respectivo parâmetro, neste caso:
 // Acordando!!
+
+const wakeUp = () => 'Acordando!';
+const breakfest = () => 'Bora tomar café';
+const sleep = () => 'Partiu dormir!!';
+
+const doingThings = (callback) => {
+    const result = callback();
+    console.log(result);
+};
+doingThings(wakeUp);
+doingThings(breakfest);
+doingThings(sleep);
