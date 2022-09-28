0f292f41a0ec5ddc0f0b143f254f73ec036eaf71
Seu nome <marcusaviotti@gmail.com>
2022-09-28 12:16:01 -0300

    Praticas do dia 5, seção 8.

---
+++ Fundamentos/Seção-8/Dia-5/Exercicios Pratica/shothand.js
@@ -0,0 +1,12 @@
+// const getPosition = (latitude, longitude) => ({
+//     latitude: latitude,
+//     longitude: longitude,
+// });
+
+// APÓS UTILIZAR DEFAULT SHOTHAND:
+
+const getPosition = (latitude, longitude) => ({latitude, longitude,});
+
+
+
+console.log(getPosition(-19.8157, -43.9542));
