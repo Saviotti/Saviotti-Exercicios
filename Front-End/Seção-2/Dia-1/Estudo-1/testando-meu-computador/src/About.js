963171897ccc6d9237496095248df3d36494d8e5
Seu nome <marcusaviotti@gmail.com>
2022-10-19 15:33:55 -0300

    Fixando aprendizados, front-end, seção 2, dia 1.

---
+++ Front-End/Seção-2/Dia-1/Estudo-1/testando-meu-computador/src/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          {/* Marcus*/}
+        </h1>
+        <p>
+          {/* Engenheiro de formação e em transição para me tornar um desenvolvedor web. */ }
+        </p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+            <li>Sou um excelente jogador de jogos eletrônicos.</li>
+            <li>Persistente.</li>
+            <li>Bem humorado.</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
